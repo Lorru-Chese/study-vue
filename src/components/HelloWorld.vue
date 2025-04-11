85107caf9ec740867aa140c43b91b5d7b95f5bdb
JackyLai327 <jackylai327@gamil.com>
2025-04-11 14:46:23 +0800

    fix: Jacky 改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,10 @@ const count = ref(0);
 const increase = () => {
   count.value++;
 }
+const display = ref({
+    name: '',
+    email: ''
+})
 
 const change = () => {
   if (count.value < 10) {
@@ -30,10 +34,16 @@ const userMail = ref("")
 const submitted = ref(false)
 
 const submit = () => {
+  console.log(userMsg.value.length)
+  console.log(userMail.value.length)
+
   if (!userMail.value.includes("@")) {
     alert('信箱怎麼會缺少@?')
     return
   }
+  display.value.name = userMsg.value
+  display.value.email = userMail.value
+
   submitted.value = true
   userMsg.value = ""
   userMail.value = ""
@@ -51,10 +61,10 @@ const submit = () => {
     <h1>一張表單在這裡</h1>
     <input v-model="userMsg" placeholder="輸入姓名">
     <input v-model="userMail" placeholder="輸入信箱">
-    <button @click = "submit">提交</button>
+    <button type="button" @click="submit" :disabled="userMsg.length <= 0 && userMail.length <= 0">提交</button>
     <div v-if = "submitted">
-      <p>姓名：{{ userMsg }}</p>
-      <p>信箱：{{ userMail }}</p>
+      <p>姓名：{{ display.name }}</p>
+      <p>信箱：{{ display.email }}</p>
     </div>
   </div>
 </template>
