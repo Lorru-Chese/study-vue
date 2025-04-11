bdbf5494357ff20bf27e18ec89c5ba494ecec9fa
Lorru <plock1125@gmail.com>
2025-04-11 14:22:33 +0800

    feat: 提交表單檢查信箱

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <form></form>
 </template>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,13 +20,23 @@ const change = () => {
   }  
 }
 
-const isActive = ref(true)
-
-
 // 同時呼叫兩種函式(程式運算上會按照排列先後順序跑程式碼，中間有錯就GG)
 const userClick = () => {
   increase();
   change();
+}
+const userMsg = ref("")
+const userMail = ref("")
+const submitted = ref(false)
+
+const submit = () => {
+  if (!userMail.value.includes("@")) {
+    alert('信箱怎麼會缺少@?')
+    return
+  }
+  submitted.value = true
+  userMsg.value = ""
+  userMail.value = ""
 }
 
 </script>
@@ -36,6 +46,16 @@ const userClick = () => {
     <p>{{ message }}</p>
     <p>計數器：{{count}}</p>
     <button @click="userClick">+1</button>
+  </div>
+  <div>
+    <h1>一張表單在這裡</h1>
+    <input v-model="userMsg" placeholder="輸入姓名">
+    <input v-model="userMail" placeholder="輸入信箱">
+    <button @click = "submit">提交</button>
+    <div v-if = "submitted">
+      <p>姓名：{{ userMsg }}</p>
+      <p>信箱：{{ userMail }}</p>
+    </div>
   </div>
 </template>
 
